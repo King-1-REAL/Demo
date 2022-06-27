cacc797760bb67977d6a2e5e2609086998caf270
King-1-REAL <kartikpendke195@gmail.com>
2022-06-27 20:30:59 +0530

    Second Commit

---
+++ src/Test.java
@@ -0,0 +1,21 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		/*int a=10;
+		int b=20;
+		int c ;
+		
+		c=a+b;
+		System.out.println ("the value of c is " +c);
+*/
+		
+		Object ref = null;
+	      //ref.toString();
+	      
+	      
+	}
+
+}
